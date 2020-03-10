1ec20854f3b1b1f1497bbc4eee905540ba3cf7ba
kiakahi7 <60090025+kiakahi7@users.noreply.github.com>
2020-03-10 13:07:38 -1000

    Update index.js

--- index.js
+++ index.js
@@ -6,51 +6,49 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//displays index.html of root path
+
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
 
-//invoked after hitting go in the html form
+
 app.post("/", function(req, res) {
     
-    // takes in the city from the html form, display in // console. Takes in as string.
-        var cityname = String(req.body.cityInput);
-        console.log(req.body.cityInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
+
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=" + units + "&APPID=" + apiKey;
+        const lat = req.body.latCoord;
+        const lon = req.body.longCoord;
+        const apiKey = "5aff7aec1194d22b9dc26e7a6e86b8d6";
+        const url = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&APPID=" + apiKey;
     
-    // this gets the data from Open WeatherPI
+
     https.get(url, function(response){
         console.log(response.statusCode);
         
-        // gets individual items from Open Weather API
+ 
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
+            const tempMin = weatherData.main.temp_min;
+            const tempMax = weatherData.main.temp_max;
             const city = weatherData.name;
-            const humidity = weatherData.main.humidity;
             const windSpeed = weatherData.wind.speed;
+            const windDirection = weatherData.wind.deg;
             const weatherDescription = weatherData.weather[0].description; 
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"; 
             
-            // displays the output of the results
+
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("Humidity is " + humidity + "% with wind speed of " + windSpeed+  " miles/hour");
+            res.write("<h2>The Minimum Temperature in " + city + " is " + tempMin + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Maximum Temperature is " + tempMax + " Degrees Fahrenheit<h2>");
+            res.write("Wind speed is " + windSpeed+  " miles/hour with a direction of " + windDirection + " Meteorological Degrees" );
             res.write("<img src=" + imageURL +">");
             res.send();
         });
     });
-})
+});
 
 
-//Commented out these lines in Repl
-//Uncomment these lines when running on laptop
 app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+console.log ("Server is running on port");
 });
